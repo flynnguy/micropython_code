499b40fd7cd7b26d9edee0a2974bffb74541131d
Christopher Flynn <flynnguy@gmail.com>
2017-12-21 22:14:57 -0500

    Use machine.unique_id for mqtt client_id

--- mqtt_example/mqtt_example.py
+++ mqtt_example/mqtt_example.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import ujson
+import ubinascii
+import machine
 from umqtt.simple import MQTTClient
 
 with open('secrets.json') as fp:
     secrets = ujson.loads(fp.read())
 
 client = MQTTClient(
-    client_id='umqtt_client',
+    client_id='esp8266_{}'.format(
+        str(ubinascii.hexlify(machine.unique_id()), 'utf-8'),
+    ),
     server=secrets['mqtt']['host'],
     user=secrets['mqtt']['user'],
     password=secrets['mqtt']['pass'],
